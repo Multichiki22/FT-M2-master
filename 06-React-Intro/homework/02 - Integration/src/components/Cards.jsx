9a76a5a0fefdd485ad26286944117234287b8e72
multichiki22 <myguelangel12@gmail.com>
2022-11-28 20:22:59 -0500

    Part 1 completed

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,6 +1,10 @@
-import Card from './Card';
+import Card from "./Card";
 
 export default function Cards(props) {
-   const { characters } = props;
-   return <div></div>;
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map((personaje) => (Card(personaje)))}
+    </div>
+  );
 }
