3ae2f801f0bce39010615540bfd737c8abce1f76
multichiki22 <myguelangel12@gmail.com>
2022-12-01 14:28:08 -0500

    React Routing fully tested

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -5,8 +5,19 @@ import NavBar from "./components/NavBar/NavBar.jsx";
 import Shipping from "./components/Shipping/Shipping.jsx";
 import Promotions from "./components/Promotions/Promotions.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
+import { Routes, Route } from "react-router-dom";
 /* eslint-disable */
 
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <NavBar path="/" />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/shipping" element={<Shipping />} />
+        <Route exact path="/promotions" element={<Promotions />} />
+        <Route exact path="/cruises/:id" element={<CardDetail />} />
+      </Routes>
+    </>
+  );
 }
--- 09-React-Routing/homework/01 - Exercises/src/components/CardDetail/CardDetail.jsx
+++ 09-React-Routing/homework/01 - Exercises/src/components/CardDetail/CardDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./CardDetail.module.css";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function CardDetail() {
   const [cruiseDetail, setCruiseDetail] = React.useState({});
@@ -14,10 +15,14 @@ export default function CardDetail() {
     return () => setCruiseDetail({});
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const backToHome = () => {
+    navigate("/");
+  };
   return (
     <div className={styles.container}>
-      <button className={styles.buttonBack}>
+      <button className={styles.buttonBack} onClick={backToHome}>
         Volver
       </button>
 
